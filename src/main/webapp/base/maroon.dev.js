166b78526cfc2d3567cbb1e216e98d56215fb28f
imakawa <bretwang@yeah.net>
2015-09-19 13:47:28 +0900

    create foundation 001

--- src/main/webapp/base/maroon.dev.js
+++
@@ -1,2 +0,0 @@
-var base = {};
-base.fn={};
--- src/main/webapp/shop/trainning/test1.js
+++
@@ -1,7 +0,0 @@
-//var container = document.querySelector('#container');
-alert('87332987');
-var msnry = new Masonry( '#container', {
-  // options
-  columnWidth: 200,
-  itemSelector: '.item'
-});
